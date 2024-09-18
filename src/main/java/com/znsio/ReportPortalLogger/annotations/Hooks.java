12180f6d8d02c48d5c180d6c1a740d2935cf6d82
Anand Bagmar <abagmar@gmail.com>
2024-09-18 18:40:20 +0530

    use ReportPortalIntegration 0.0.4 - which has the ReportPortalListener as a singleton

--- src/main/java/com/znsio/ReportPortalLogger/annotations/Hooks.java
+++ src/main/java/com/znsio/ReportPortalLogger/annotations/Hooks.java
@@ -1,8 +1,14 @@
 package com.znsio.ReportPortalLogger.annotations;
 
+import com.znsio.reportportal.integration.listener.ReportPortalListener;
 import org.testng.annotations.BeforeSuite;
 
 public class Hooks {
+
+    public Hooks() {
+        ReportPortalListener.getInstance();
+    }
+
     @BeforeSuite
     public void beforeSuite() {
         System.out.println("Before Suite");
