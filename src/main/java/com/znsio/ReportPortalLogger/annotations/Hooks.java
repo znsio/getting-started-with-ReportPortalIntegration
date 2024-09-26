76315aa8c28df4e2117b523a5b39de86b63b9406
Anand Bagmar <abagmar@gmail.com>
2024-09-26 13:43:34 +0530

    updated ReportPortalLogger to version 0.0.5

--- src/main/java/com/znsio/ReportPortalLogger/annotations/Hooks.java
+++ src/main/java/com/znsio/ReportPortalLogger/annotations/Hooks.java
@@ -1,16 +1,8 @@
 package com.znsio.ReportPortalLogger.annotations;
 
 import com.znsio.reportportal.integration.listener.ReportPortalListener;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 
+@Listeners(ReportPortalListener.class)
 public class Hooks {
-
-    public Hooks() {
-        ReportPortalListener.getInstance();
-    }
-
-    @BeforeSuite
-    public void beforeSuite() {
-        System.out.println("Before Suite");
-    }
 }
